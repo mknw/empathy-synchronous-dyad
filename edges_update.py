093df6d3929e765044646636badc0724bc8e3ef6
Michael <onvergetelijkontbijt@icloud.com>
2018-06-08 12:48:23 +0200

    Plot improved with states and adaptive weights in subplots

--- edges_update.py
+++ edges_update.py
@@ -9,11 +9,11 @@ The attribute state in every node defines the last state of the node. At the dic
 evolution of the values for the state over time.
 '''
 def edges_update(graph, t, delta = 0.2):
-   
+
     # set dictionary keys to a meaningful output relative to
     # the time input (Xpressd in secs)
 #    t = round(t*delta, 1)
-    
+
 #    if t % 2 == 0:
 #        print('Time: ', t)
 
@@ -25,21 +25,20 @@ def edges_update(graph, t, delta = 0.2):
         old_weight = g.get_edge_data(source_node,target_node)[0]['weight']
         source = g.node[source_node]['state']
         target = g.node[target_node]['state']
-        
+
         #calculate weight according to attributes extracted from nx graph
         #speed_factor is different from states' speed_factor, as it's an attribute
         #  of the edge and not of the state
         if 'hebbian' in g[source_node][target_node][0]:
 #            print('hebbian:' + source_node + target_node)
-#            function = 'hebbian' # unnecessary
-            speed_factor = g[source_node][target_node][0]['speed_factor'] 
+            speed_factor = g[source_node][target_node][0]['speed_factor']
+            # speed_factor = 0.1
             persistence = g[source_node][target_node][0]['persistence']
-
-            variation = speed_factor * (source * target * (1 - target) + persistence * target)
-            # temporarily changed to:
-            #variation = 0.5
+            # persistence = 5
+            variation = speed_factor * (source * target * (1 - target) + persistence * target
+            # variation = 0.6
 #            variation = speed_factor * (source * target - target**2 * old_weight) * persistence
-            
+
 
         elif 'slhom' in g[source_node][target_node][0]:
 #            print('slhomophily:' + source_node + target_node)
@@ -48,7 +47,7 @@ def edges_update(graph, t, delta = 0.2):
             thres_h = g[source_node][target_node][0]['thres_t']
             amplification = g[source_node][target_node][0]['amplification']
             variation = old_weight + amplification * old_weight * (1 - old_weight) * (thres_h - np.abs(source - target))
-            
+            # import ipdb; ipdb.set_trace()
         else: #check if function str is falsy
             # feeds old_weight instead of new_weight to the weightTimeLine
             g[source_node][target_node][0]['weightTimeLine'].update({t:old_weight})
@@ -76,14 +75,14 @@ def edges_update(graph, t, delta = 0.2):
 #        elif function == 'advanced_quadratic':
 #            if amplification == None:
 #                print('Error! Amplification not set!')
-#                exit(0)
+#                exit(0))
 #
 #            variation = (old_weight + amplification * ((1 - old_weight) * (np.abs(thres_h^2 - np.abs(source - target)^2) + (
 #            thres_h ^ 2 - np.abs(source - target) ^ 2)) / 2 + old_weight * (np.abs(thres_h ^ 2 - np.abs(source - target) ^ 2) - (
 #                                                           thres_h ^ 2 - np.abs(source - target) ^ 2)) / 2))
 
 #        else:
-#            
+#
 #            exit(0)
 
         new_weight = old_weight + speed_factor * (variation - old_weight )*delta # homophily model
@@ -92,14 +91,13 @@ def edges_update(graph, t, delta = 0.2):
         #new_weight = new_weight if new_weight <= 1 else 1
 
         try:
-            
             g[source_node][target_node][0]['weightTimeLine'].update({t:new_weight})
         except:
             print(t, source, target, g[source][target]['weightTimeLine'])
-            
+
         try:
             g[source_node][target_node][0]['weight'] = np.asscalar(new_weight)
         except:
             g[source_node][target_node][0]['weight'] = new_weight
-            
+
     return g
--- import_data.py
+++ import_data.py
@@ -24,14 +24,13 @@ class syncNet(object):
       self.sheet2 = template[1]
       #states'_names
       self.sts_nms = np.asarray(self.sheet1.iloc[0:1, 0:9])[0]
-   
 
    def import_model(self):
       '''this function turns the sheet template into readable pandas dataframes'''
-         
+
       # scrape weights into dataframe:
       weights_clean = np.asmatrix(self.sheet1.iloc[2:11, 0:9])
-      weights_df = pd.DataFrame(weights_clean, index = self.sts_nms, 
+      weights_df = pd.DataFrame(weights_clean, index = self.sts_nms,
                                 dtype="Float64")
       # label weights indices:
       weights_df.columns = self.sts_nms
@@ -49,30 +48,30 @@ class syncNet(object):
       comb_par_df = self.sheet1.iloc[15:28, 0:9]
       comb_par_df.columns = self.sts_nms
       ##
-      
+
       # scrape heb and hom parameters from sheet 2 into df
       adcon_par_df = self.sheet2.iloc[2:11, 1:101]
-      
+
       # scrape parameter names...
       par_ar = np.asarray(self.sheet2.iloc[2:11, 0:1])
       #...and assign them to parameter indices
       par_nms = []
-      for pn in par_ar: #remove unwanted tuples caused by comas 
+      for pn in par_ar: #remove unwanted tuples caused by comas
          par_nms.append(''.join(pn[0]))
       adcon_par_df.index = par_nms
-      
+
       # create state tuples, e.g. ('X1', 'x1') ... ('X10', 'x10')
       tot_sts_nms = np.append(self.sts_nms, 'X10')
       orig_sts = [stt for stt in tot_sts_nms for y in range(10)]
       dest_sts = [stt for stt in tot_sts_nms] * 10
-      sts_tpls = zip(orig_sts, dest_sts) 
+      sts_tpls = zip(orig_sts, dest_sts)
       # finally, assign cols to hebbian and homophily parameters
       adcon_par_df.columns = sts_tpls
-      
+
       # and drop unused columns
       adcon_par_df = adcon_par_df.dropna(1, 'all')
       ##
-      
+
       #assign df to syncNet
       self.weights_df = weights_df
       self.speed_factors_df = speed_factors_df
@@ -80,9 +79,9 @@ class syncNet(object):
       self.adcon_par_df = adcon_par_df
       self.init_states = [float(n) for n in[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
       return
-   
+
    def build_dyad(self):
-      '''method to shape the graph's main structure and 
+      '''method to shape the graph's main structure and
       makes each edge and vertex ready for the 2 edges_/states_update functions'''
       dyad = nx.from_numpy_matrix(self.weights_df.values, create_using=nx.MultiDiGraph())
       old_new = dict(zip(dyad.nodes, self.sts_nms))
@@ -91,33 +90,33 @@ class syncNet(object):
       #timeline creation
       stt_d = {} #create dictionary for the 'state' attribute to be used by update functions
       stt_tmln_d = {} #create timelines dictionary for state activities
-      
+
       for i in range(len(self.sts_nms)):
          #match states' names with initial weights
-         stt_d[self.sts_nms[i]] = self.init_states[i] 
+         stt_d[self.sts_nms[i]] = self.init_states[i]
          # match time 0 with initial weight; assign it to states' names
          d = {0: self.init_states[i]} # time = 0 for all dicts
-         stt_tmln_d[self.sts_nms[i]] = d 
-         
-         
+         stt_tmln_d[self.sts_nms[i]] = d
+
+
       # push into graph
       nx.set_node_attributes(dyad, stt_tmln_d, 'activityTimeLine')
       nx.set_node_attributes(dyad, stt_d, 'state')
-      
+
 #      for x in range(len(sts_mns)):
 #         for y in range(len(sts_mns)):
 #            weight = (sync_dyad.weights_df[sts_mns[y]][sts_mns[x]])
-      
-      
+
+
       for orig, dest in dyad.edges():
          weight = dyad.get_edge_data(orig, dest)[0]['weight']
          dyad[orig][dest][0].update(weightTimeLine={0:weight})
-            
+
       self.dyad = dyad
       return
-   
+
    def plug_parameters(self):
-      '''iterate over pandas dataframes and plug respective values into 
+      '''iterate over pandas dataframes and plug respective values into
       networkx graph. Added attributes are to be used by the eu & su functions'''
       #list of edge_update parameters:
       eu_par = ['speed_factor', 'hebbian', 'persistence', 'slhom', 'alhom',
@@ -128,26 +127,26 @@ class syncNet(object):
 
 
       ######### update EDGES attributes ##############
-      adcon_par_df = self.adcon_par_df 
+      adcon_par_df = self.adcon_par_df
       adcon_par_df.index = eu_par # change levels to the ones used as function arguments
 
       for c in adcon_par_df.columns:
          for r in adcon_par_df.index:
             if adcon_par_df.loc[r][c] == adcon_par_df.loc[r][c]: # =if not NaN
-               
+
                value = adcon_par_df.loc[r][c]
 #               print("nodes: {}, Par:{}, Value:{}".format(c, r, value))
-               
+
                # find edge by indexing the dataframe column (tuple), update its value:
                set_edge_attr = "self.dyad[c[0]][c[1]][0].update({}=value)".format(r)
                exec(set_edge_attr)
-         # a bit of feedback:      
+         # a bit of feedback:
          print('Edge for nodes: {} saved to nx graph'.format(c))
-         
+
       ########### update VERTICES attributes ###############
       comb_par_df = self.comb_par_df
       comb_par_df.index = su_par # change levels to the ones used as function arguments
-      
+
       for r in comb_par_df.index:
          comb_d = {}
          uptd_nds = []
@@ -155,13 +154,13 @@ class syncNet(object):
             if comb_par_df.loc[r][c] == comb_par_df.loc[r][c]: # =if not NaN
                value = comb_par_df.loc[r][c] # save assign param to 'value'
                # assign the par value to 'r' key, which is the state's_name in the dict
-               comb_d[c] = value 
+               comb_d[c] = value
                uptd_nds.append(c)
-               
+
          nx.set_node_attributes(self.dyad, comb_d, r) # 'r' is the name of the attribute
          if uptd_nds:
             print("\"{}\" attribute updated for nodes: {} in nx graph".format(r, uptd_nds))
-      
+
       ########## plug speed factors in states ############
       speed_factors_df = self.speed_factors_df
       spf_d = {}
@@ -173,48 +172,72 @@ class syncNet(object):
 
    def record_interaction(self, time=100, delta=0.2):
       dyad = self.dyad
-      
+
       for t in range(1, time): # OC original code
          temp_dyad = edges_update(dyad, t, delta)
          dyad = states_update(temp_dyad, t, delta)
-         
+
 #      for t in range(1, time): # update only states
 #         dyad = states_update(dyad, t, delta)
       print("Adaptive Timeline created for: " + str(self.name))
       self.dyad = dyad # networkx graph
       return
-   
+
+   def get_states_dict(self):
+       real_names = [r"$ws_s$",
+                     r"$srs_{A, s}$", r"$srs_{A, esB}$", r"$ps_{A, a}$", r"$es_{A, a}$",
+                     r"$srs_{B, s}$", r"$srs_{B, esA}$", r"$ps_{B, a}$", r"$es_{B, a}$"]
+       real_names_dict = dict(zip(self.sts_nms, real_names))
+       return real_names_dict
+
    def plot_activation(self):
-      plt.figure(figsize=(20,10))
+      plt.figure(figsize=(20,15))
+      plt.suptitle("Node and weight timelines for the " + self.name, fontsize = 25)
+      plt.subplot(2, 1, 1)
       for node in self.dyad.nodes():
          state_tuples = self.dyad.node[node]['activityTimeLine'].items()
          plt.plot(*zip(*state_tuples))
-         
-      plt.legend(self.sts_nms)
-      plt.show()
+
+      plt.ylabel(r"Activation level $_{(normalised)}$", fontsize=15)
+      plt.xlabel("Time (seconds)", fontsize=15)
+      plt.title("Node activation", {'fontsize': 20})
+      legend = [r"$ws_s$",
+                r"$srs_{A, s}$", r"$srs_{A, esB}$", r"$ps_{A, a}$", r"$es_{A, a}$",
+                r"$srs_{B, s}$", r"$srs_{B, esA}$", r"$ps_{B, a}$", r"$es_{B, a}$"]
+      plt.legend(legend, loc=2, fontsize='x-large')
+      # plt.show()
       print("Plotted vertices:")
       print(self.sts_nms)
       return
-   
-   
+
+
    def plot_weights(self):
-      plt.figure(figsize=(20, 10))
-      adcon_list = []
-      
+      plt.subplot(2, 1, 2)
+      rl_adcon_list = []
+      names_dict = self.get_states_dict()
       for edge in self.dyad.edges():
          source, target = edge
          #Select only adaptive edges based on number of attr assigned:
-#         if len(self.dyad.get_edge_data(source, target)[0]) > 3:
-         if len(self.dyad[source][target][0]) > 3:
+         # if len(self.dyad.get_edge_data(source, target)[0]) > 3:
+         if len(self.dyad[source][target][0]) > 4:
             #get items:
             state_tuples = self.dyad.get_edge_data(source, target)[0]['weightTimeLine'].items()
             #unpack them:
             plt.plot(*zip(*state_tuples))
-            adcon_list.append(edge)
-            
-      plt.legend(adcon_list)
+
+            # create legend through node names dictionary
+            real_edge = str(names_dict[source] + ", " + names_dict[target])
+            rl_adcon_list.append(real_edge)
+
+
+      plt.ylabel(r"Weights $_{(normalised)}$", fontsize=15)
+      plt.xlabel("Time (seconds)", fontsize=15)
+      plt.title("Edges weights", {'fontsize': 20})
+      plt.legend(rl_adcon_list, loc=3, fontsize='x-large')
+      plt.tight_layout(pad=6)
+
       plt.show()
-      print("Plotted edges:" + str(adcon_list))
+      print("Plotted edges:" + str(rl_adcon_list))
       return
 
 
@@ -226,56 +249,56 @@ if __name__ == '__main__':
    sync_dyad.record_interaction(time=30, delta=0.2)
    sync_dyad.plot_timeline()
 
-
-'''testing functions'''
-#### NODES COLLECTION
-#sync_dyad.dyad.nodes()
-##
-### EDGES COLLECTION
-#sync_dyad.dyad.edges()
 #
-## FOR EACH NODE print NAME + NX NODE attribute 
-#for vrtx in sync_dyad.dyad.nodes():
+# '''testing functions'''
+# ### NODES COLLECTION
+# sync_dyad.dyad.nodes()
+# #
+# ## EDGES COLLECTION
+# sync_dyad.dyad.edges()
+#
+# # FOR EACH NODE print NAME + NX NODE attribute
+# for vrtx in sync_dyad.dyad.nodes():
 #   print(vrtx)
 #   print(sync_dyad.dyad.node[vrtx])
 #
-##
-#### for each EDGE print NAME, SUCCESSOR + EDGE attribute
-##print(list(sync_dyad.dyad.predecessors('X1')))
-#for vrtx in sync_dyad.dyad.nodes():
+# #
+# ### for each EDGE print NAME, SUCCESSOR + EDGE attribute
+# #print(list(sync_dyad.dyad.predecessors('X1')))
+# for vrtx in sync_dyad.dyad.nodes():
 #   for succ in sync_dyad.dyad.successors(vrtx):
 #      print(vrtx, succ)
 #      print(sync_dyad.dyad[vrtx][succ])
 #
 #
-### get specific NODE 
-#sync_dyad.dyad.node['X8']
+# ## get specific NODE
+# sync_dyad.dyad.node['X8']
 #
-### get specific EDGE
-#sync_dyad.dyad.get_edge_data('X2', 'X4')[0]
+# ## get specific EDGE
+# sync_dyad.dyad.get_edge_data('X2', 'X4')[0]
 #
-##
+# #
 #
-#sync_dyad.dyad['X3']['X4'][0]['weightTimeLine']
+# sync_dyad.dyad['X3']['X4'][0]['weightTimeLine']
 #
-### print NAME + edgeFUNCTION
-#g = sync_dyad.dyad
-#for e in g.edges():
+# ## print NAME + edgeFUNCTION
+# g = sync_dyad.dyad
+# for e in g.edges():
 #   orig, dest = e
 #   print("from " + orig + " to " + dest)
-#   
+#
 #   if 'hebbian' in g[orig][dest][0]:
 #      print("hebbian")
 #   elif 'slhom' in g[orig][dest][0]:
 #      print("slhom")
 #   else:
 #      print("no change in weight")
-#      
-### UPDATE function, works only when assigning sync_dyad.dyad to a var (g)
-#vrtx = 'X7'
-#t = 30
-#actual_state = 1
-#g.node[vrtx]['activityTimeLine'].update({t: actual_state})
-#g.node['X7']['activityTimeLine'][30] = 0.5
-#g.get_edge_data(source_node,target_node)[0]['weight'] = np.asscalar(new_weight) 
-###################################
+#
+# ## UPDATE function, works only when assigning sync_dyad.dyad to a var (g)
+# vrtx = 'X7'
+# t = 30
+# actual_state = 1
+# g.node[vrtx]['activityTimeLine'].update({t: actual_state})
+# g.node['X7']['activityTimeLine'][30] = 0.5
+# g.get_edge_data(source_node,target_node)[0]['weight'] = np.asscalar(new_weight)
+# ##################################
